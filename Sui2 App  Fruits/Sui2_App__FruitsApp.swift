5d845a92afbed6c0a33c6df302f8987a41e63ed6
Harry Lopez <harry.lopez@overactive.com>
2022-04-06 15:45:13 -0500

    Initial Commit

---
+++ Sui2 App  Fruits/Sui2_App__FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sui2_App__FruitsApp.swift
+//  Sui2 App  Fruits
+//
+//  Created by Harry Lopez on 6/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Sui2_App__FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
